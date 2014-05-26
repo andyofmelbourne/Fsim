fcf48a712cfad82a9f239d1ea0a9a3c8e062a8dc
andyofmelbourne <andyofmelbourne@gmail.com>
2014-05-26 15:43:14 +0200

    simulate cubes of Gauss' for the cfel logo

--- cubeOguas.py
+++ cubeOguas.py
@@ -19,6 +19,25 @@ def main(argv):
     print 'pos is ', pos
     return pos
 
+def update_progress(progress):
+    barLength = 20 # Modify this to change the length of the progress bar
+    status = ""
+    if isinstance(progress, int):
+        progress = float(progress)
+    if not isinstance(progress, float):
+        progress = 0
+        status = "error: progress var must be float\r\n"
+    if progress < 0:
+        progress = 0
+        status = "Halt...\r\n"
+    if progress >= 1:
+        progress = 1
+        status = "Done...\r\n"
+    block = int(round(barLength*progress))
+    text = "\rPercent: [{0}] {1}% {2}".format( "#"*block + "-"*(barLength-block), int(progress*100), status)
+    sys.stdout.write(text)
+    sys.stdout.flush()
+
 def rotation_matrix(axis,theta):
     axis = axis/math.sqrt(np.dot(axis,axis))
     a = math.cos(theta/2)
@@ -27,38 +46,26 @@ def rotation_matrix(axis,theta):
                      [2*(b*c+a*d), a*a+c*c-b*b-d*d, 2*(c*d-a*b)],
                      [2*(b*d-a*c), 2*(c*d+a*b), a*a+d*d-b*b-c*c]])
 
-def run(pos = 0, shape = (2048, 2048), angles = [0.0, 0.0, 0.0], unit_tile = [5,5,5], unit_dim = [10.0e-10, 10.0e-10, 10.0e-10]):
+def run(pos = 0, shape = (512, 512), angles = [0.0, 0.0, 0.0], unit_tile = [3,3,3], unit_dim = [10.0e-10, 10.0e-10, 10.0e-10], sigma = 1.0):
     # Geometry stuff
-    X         = [1000.0e-10, 1000.0e-10]
+    X         = [100.0e-10, 100.0e-10]
     spacing_x = np.array(X) / np.array(shape, np.float64) 
     spacing_q = 1 / np.array(X)
-
     h         = 6.62606957e-34
     c         = 299792458.0
     E_ev      = 22.0e3
     E         = E_ev * 1.60217657e-19
     lamb      = c * h / E
-
     du        = 55.0e-6
-    #Z         = du * shape[0] / (2.0 * theta_max)
     Z         = du * np.array(X).max()/ lamb 
     theta_max = np.arctan(du * shape[0] / (2 * Z))
     Q_max     = theta_max / lamb    
 
+    #
     # Make a list of atomic sites
-    # Make a unit cell of 
-    #number_of_atoms_unit = 3
-    #unit   = np.random.rand(number_of_atoms_unit, 3)
     #cubic
     unit = []
     unit.append([0, 0, 0])
-    #unit.append([1, 0, 0])
-    #unit.append([0, 1, 0])
-    #unit.append([1, 1, 0])
-    #unit.append([0, 0, 1])
-    #unit.append([1, 0, 1])
-    #unit.append([0, 1, 1])
-    #unit.append([1, 1, 1])
 
     # unit cell dimensions
     unit_x = 10.0e-10
@@ -82,38 +89,19 @@ def run(pos = 0, shape = (2048, 2048), angles = [0.0, 0.0, 0.0], unit_tile = [5,
     atomic_sites[:, 1] -= atomic_sites[:, 1].mean() + pos * unit_x
     atomic_sites[:, 2] -= atomic_sites[:, 2].mean()
 
-    # Rotate the atomic sites about the z-axis
-    # Randomply select orientation
-    # theta_x, theta_y, theta_z = 0.125 * np.pi * np.random.random(3)
+    # Rotate the atomic sites about the xyz-axis
     theta_x, theta_y, theta_z = angles
-    #theta_z = np.pi/3
-    #theta_x = np.pi/1.5
-    #theta_y = np.pi/10
     atomic_sites = np.transpose( np.dot(rotation_matrix(np.array([0, 0, 1]), theta_z), np.transpose(atomic_sites)) )
     atomic_sites = np.transpose( np.dot(rotation_matrix(np.array([0, 1, 0]), theta_x), np.transpose(atomic_sites)) )
     atomic_sites = np.transpose( np.dot(rotation_matrix(np.array([1, 0, 0]), theta_y), np.transpose(atomic_sites)) )
 
-    # make a convergent beam illumination
-    qx, qy = bg.make_xy(shape, spacing=[1/X[0], 1/X[1]])
-    q      = np.sqrt(qx**2 + qy**2)
-    q_max  = 30.0e-3 / lamb
-    mask   = (q < q_max)
-    probeF = np.array(mask, dtype=np.complex128)
-    # add defocus
-    df     = 3000.0e-10
-    qy, qx = bg.make_xy(shape, origin=[0,0], spacing = [1/X[0], 1/X[1]])
-    expF   = np.exp(-1J * np.pi * lamb * df * (qx**2 + qy**2))
-    probeF *= expF
-    probeR = bg.ifft2(probeF)
-
-    def probe_z(z):
-        expF = np.exp(-1J * np.pi * lamb * z * (qx**2 + qy**2))
-        return bg.ifft2(probeF * expF)
-
     # make the potential at an atomic site (Fourier space)
     interaction = 1.0e-1
-    qy2, qx2 = bg.make_xy(shape, origin=[0,0], spacing = [1.0e-10/X[0], 1.0e-10/X[1]])
-    V_f      = interaction * np.exp(- np.pi**2 * 4.0 * (qy2**2 + qx2**2))
+    spacing  = [1.0e-10/X[0], 1.0e-10/X[1]]
+    qy2, qx2 = bg.make_xy(shape)
+    qy2      = qy2 * spacing[0]
+    qx2      = qx2 * spacing[1]
+    V_f      = interaction * np.exp(- np.pi**2 * 2.0 * sigma**2 * (qy2**2 + qx2**2))
 
     n, m = bg.make_xy(shape)
     def V_yxz(atomic_site):
@@ -121,26 +109,28 @@ def run(pos = 0, shape = (2048, 2048), angles = [0.0, 0.0, 0.0], unit_tile = [5,
         yi = atomic_site[0]/spacing_x[0]
         if np.abs(xi) > shape[1]/2 or np.abs(yi) > shape[0]/2 :
             return 0.0
-        ramp = np.exp(-2J * np.pi * (n * yi / shape[0] + m * xi / shape[1]))
+        ramp = np.exp(-2J * np.pi * (n * yi / float(shape[0]) + m * xi / float(shape[1])))
         return 1J*bg.ifft2(ramp * V_f)
-
         
     V_proj = np.zeros(shape, np.complex128)
     exitF  = np.zeros(shape, np.complex128)
     for i in range(len(atomic_sites)):
-        print len(atomic_sites), i
-        #probe_i = probe_z(atomic_sites[i][2])
+        update_progress(i / float(len(atomic_sites) - 1))
         V_i     = V_yxz(atomic_sites[i])
-        #exitF  += bg.fft2(V_i * probe_i)
         V_proj += V_i
-    #exitF  += probeF
     #
     return V_proj
 
 if __name__ == '__main__':
-    pos = 0
-    angles_xyz = [0.0, 0.0, 0.0]
-    V_proj = run(pos, angles)
-    #
-    # output
-    #bg.binary_out(V_proj, 'V_proj_'+str(pos)+'_'+str(theta_x)+'_'+str(theta_y)+'_'+str(theta_z), dt=np.complex128, appendDim=True) 
+    tile   = np.arange(2, 10) 
+    unit_tile = np.array([0, 0, 0])
+    sigma  = np.linspace(0.0, 2.0, 5)
+    # [111] axis 
+    angles = [np.pi / 4.0, math.acos(np.sqrt(6.0) / 3.0), 0.0]
+    V_proj = []
+    for t in tile:
+        for sig in sigma:
+            print t, sig
+            unit_tile.fill(t)
+            V_proj.append(run(angles = angles, unit_tile=unit_tile, sigma = sig))
+
